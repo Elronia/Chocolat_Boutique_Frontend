6837918b436946da6f31c664ab1a01f8d561786f
Elronia <zar.catherine@gmail.com>
2020-10-17 23:03:35 -0400

    working on components

--- chocolat/src/Account.js
+++ chocolat/src/Account.js
@@ -1,3 +1,3 @@
-import React from 'react'
+// import React from 'react'
 
-export default Account;
+// export default Account;
--- chocolat/src/Cart.js
+++ chocolat/src/Cart.js
@@ -1,3 +1,3 @@
-import React from 'react'
+// import React from 'react'
 
-export default Cart;
+// export default Cart;
